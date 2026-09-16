f192be70e278ef0d256ba5e6207dd6a723abe8e5
PatelVishakha-07 <vishakhapatel8270@gmail.com>
2026-09-16 19:59:06 +0530

    implemented multilevel inheritance

---
+++ inheritance.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="inheritance.aspx.cs" Inherits="registration.inheritance" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Length: <asp:TextBox ID="txtlength" runat="server" TextMode="Number"></asp:TextBox> <br /><br />
+            Width: <asp:TextBox ID="txtwidth" runat="server" TextMode="Number"></asp:TextBox> <br /><br />
+            Radius: <asp:TextBox ID="txtrad" runat="server" TextMode="Number"></asp:TextBox> <br /><br />
+
+            <asp:Button ID="btndisplay" runat="server" Text="Display" OnClick="btndisplay_Click" /> <br /><br />
+
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ inheritance.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace registration
+{
+    public partial class inheritance : System.Web.UI.Page
+    {
+        public class Shape
+        {
+            public string name;
+            public Shape(string name)
+            {
+                this.name = name;
+            }
+
+            public string display()
+            {
+                return name;
+            }
+        }
+
+        public class Rectangle : Shape
+        {
+            int length, width;
+            public Rectangle(int length, int width): base("Rectangle")
+            {
+                this.length = length;
+                this.width = width;
+            }
+
+            public int calculate_perimeter_rectangle()
+            {
+                return 2 * length + 2 * width;
+            }
+        }
+
+        public class Circle : Shape{
+            int radius;
+            public Circle(int radius) : base("Circle")
+            {
+                this.radius = radius;
+            }
+
+            public double calculate_perimeter_circle()
+            {
+                return 2 * 3.14 * radius;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btndisplay_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Rectangle r = new Rectangle(int.Parse(txtlength.Text), int.Parse(txtwidth.Text));
+            Label1.Text += "Shape Name: " + r.display();
+            Label1.Text += "<br>Perimeter of Rectangle: " + r.calculate_perimeter_rectangle();
+
+            Circle c = new Circle(int.Parse(txtrad.Text));
+            Label1.Text += "<br><br>Shape Name: " + c.display();
+            Label1.Text += "<br>Perimeter of Circle: " + c.calculate_perimeter_circle();
+
+        }
+    }
+}
