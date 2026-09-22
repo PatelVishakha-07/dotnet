ee2be2b424f9011dffd831c32b3ed3077ee973d9
PatelVishakha-07 <vishakhapatel8270@gmail.com>
2026-09-22 22:24:39 +0530

    tuple code implemented

---
+++ tuple.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tuple.aspx.cs" Inherits="registration.tuple" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name: <asp:TextBox ID="txtname" runat="server"></asp:TextBox> <br /><br />
+            Age:<asp:TextBox ID="txtage" runat="server"></asp:TextBox><br /><br />
+            Course: <asp:TextBox ID="txtcourse" runat="server"></asp:TextBox><br /><br />
+
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" /> <br /><br />
+
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ tuple.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace registration
+{
+    public partial class tuple : System.Web.UI.Page
+    {
+        private List<Tuple<string, int, string>> person
+        {
+            get
+            {
+                if (ViewState["person"] == null)
+                    ViewState["person"] = new List<Tuple<string, int, string>>();
+                return (List<Tuple<string, int, string>>)ViewState["person"];
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var p = Tuple.Create(txtname.Text, Convert.ToInt32(txtage.Text), txtcourse.Text);
+            person.Add(p);
+
+            string data = "";
+            foreach (var item in person)
+            {
+                data += $"Name: {item.Item1}, Age: {item.Item2}, Course: {item.Item3}<br />";
+            }
+            Label1.Text = data;
+        }
+    }
+}
